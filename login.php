59cc2e07aabfc9ede1003433a3b754e413859647
noeavila42 <a22021483@iteshu.edu.mx>
2024-11-26 03:31:11 -0600

    Update login.php

--- login.php
+++ login.php
@@ -26,14 +26,20 @@ $result = $stmt->get_result();
 
 // Verificar si los datos son correctos
 if ($result->num_rows > 0) {
-    // Redirigir a bienvenida.html si el inicio de sesión es exitoso
-    header("Location: inicio.html");
+    // Verificar si es administrador
+    if ($email === "any@iteshu.edu.mx" || $email === "any@iteshu.edu.mx") {
+        // Redirigir al panel de administrador
+        header("Location: resul.php");
+    } else {
+        // Redirigir al panel de usuario
+        header("Location: inicio.html");
+    }
     exit();
 } else {
     // Mostrar mensaje de error y redirigir a la página de inicio de sesión
     echo "<script>
-
-        window.location.href = 'error.html';
+        alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
+        window.location.href = 'login.html';
     </script>";
 }
 
